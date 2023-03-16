4e218f9a6bf51d4c1be369fab11cbe56bc1a4108
Stanley <78428936+chepkok3@users.noreply.github.com>
2023-03-16 19:37:54 +0300

    Add myprofile and missions jest tests

--- src/test/myprofile.test.js
+++ src/test/myprofile.test.js
@@ -1,17 +1,17 @@
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Header from '../components/header';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import store from '../reduxState/store';
+import Profile from '../components/my_profile';
 
-it('Header page renders correctly', () => {
+it('renders correctly', () => {
   const tree = renderer
     .create(
       <React.StrictMode>
         <Provider store={store}>
           <BrowserRouter>
-            <Header />
+            <Profile />
           </BrowserRouter>
         </Provider>
       </React.StrictMode>,
