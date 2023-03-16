95c3a120710aedee1b63f3dc16bd3db5726f93b2
Stanley <78428936+chepkok3@users.noreply.github.com>
2023-03-16 11:36:35 +0300

    Add display joined missions and switch badges for missions

--- src/components/my_profile.js
+++ src/components/my_profile.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { myReservedMissions } from '../reduxState/missionSlice';
+import styles from './styles/myprofile.module.css';
 
-const MyProfile = () => <div>{/* <h1>MyProfile</h1> */}</div>;
+function MyProfile() {
+  const dispatch = useDispatch();
+  const reservedMission = useSelector((store) => store.missions.reserved);
+
+  useEffect(() => {
+    dispatch(myReservedMissions());
+  }, [dispatch]);
+
+  let renderReservedMissions = '';
+
+  if (reservedMission.length) {
+    renderReservedMissions = reservedMission.map((mision) => (
+      <li key={mision.mission_id}>
+        <span>{mision.mission_name}</span>
+      </li>
+    ));
+  }
+
+  return (
+    <div className={styles.missionProfile}>
+      <div className={styles.missionContainer}>
+        <h1>My Missions</h1>
+        <ul>{renderReservedMissions}</ul>
+      </div>
+    </div>
+  );
+}
 
 export default MyProfile;
